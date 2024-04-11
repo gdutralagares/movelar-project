ab8e90466e6ef6ed9af64f8ac1bee6309daac6c1
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-04-11 15:50:37 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+  menu.classList.toggle("move");
+  cart.classList.remove("active");
+  login.classList.remove("active");
+};
+
+//cart toggle
+let cart = document.querySelector(".cart");
+document.querySelector("#cart-icon").onclick = () => {
+  cart.classList.toggle("active");
+  navbar.classList.remove("active");
+  menu.classList.remove("move");
+  login.classList.remove("active");
+};
+//login form
+let login = document.querySelector(".login-form");
+document.querySelector("#user-icon").onclick = () => {
+  login.classList.toggle("active");
+  cart.classList.remove("active");
+  navbar.classList.remove("active");
+  menu.classList.remove("move");
+};
